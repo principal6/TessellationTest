141be47851efff42f9b9a0b599617beffadf1a40
Jesus Kim <42127220+principal6@users.noreply.github.com>
2019-10-18 23:47:25 +0900

    Update DSBase.hlsl

--- Shader/DSBase.hlsl
+++ Shader/DSBase.hlsl
@@ -10,10 +10,10 @@ DS_OUTPUT main(HS_CONSTANT_DATA_OUTPUT TessFactors, float3 Domain : SV_DomainLoc
 {
 	DS_OUTPUT Output;
 	
-	Output.Position = Patch[0].WorldPosition * Domain.x + Patch[1].WorldPosition * Domain.y + Patch[2].WorldPosition * Domain.z;
+	Output.WorldPosition = Patch[0].WorldPosition * Domain.x + Patch[1].WorldPosition * Domain.y + Patch[2].WorldPosition * Domain.z;
+	Output.Position = Output.WorldPosition;
 	Output.Position = mul(float4(Output.Position.xyz, 1.0f), VP);
 
-	Output.WorldPosition = Patch[0].WorldPosition * Domain.x + Patch[1].WorldPosition * Domain.y + Patch[2].WorldPosition * Domain.z;
 	Output.Color = Patch[0].Color * Domain.x + Patch[1].Color * Domain.y + Patch[2].Color * Domain.z;
 	Output.UV = Patch[0].UV * Domain.x + Patch[1].UV * Domain.y + Patch[2].UV * Domain.z;
 	Output.WorldNormal = Patch[0].WorldNormal * Domain.x + Patch[1].WorldNormal * Domain.y + Patch[2].WorldNormal * Domain.z;
